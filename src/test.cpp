d1ef596c78012a68a602f61da1881143d0bd79d4
sammyhasan17 <sammyhasan17@gmail.com>
2025-10-30 20:28:19 -0400

    C++ running on Windows

---
+++ src/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World in C++!" << std::endl;
+    return 0;
+}
+
+
+// note: this requires:
+
+//VSCode C++ extension
+// C++ Compiler minGW installed from MSYS2
+
+//https://code.visualstudio.com/docs/cpp/config-mingw#_prerequisites
